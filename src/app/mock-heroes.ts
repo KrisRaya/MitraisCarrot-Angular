af1f70bb31714a20ff677468a306232a2ab22d56
jafarshodiqyb <jafarshodiqyb@gmail.com>
2018-04-24 08:26:05 +0800

    First Commit, Until 7th Routing example

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Saras 008', gender: 'f'},
+    { id: 12, name: 'Panji Manusia Millenium', gender: 'm'},
+    { id: 13, name: 'Wiro Sableng', gender: 'm'},
+    { id: 14, name: 'Srikandi',gender:'f'},
+    { id: 15, name: 'Gundala',gender:'m'},
+    { id: 16, name: 'Komodo',gender: 'f'},
+    { id: 17, name: 'Sri asih', gender: 'f'},
+    { id: 18, name: 'Bhima',gender: 'f'},
+    { id: 19, name: 'Godam',gender:'m'},
+    { id: 20, name: 'Valentine',gender:'f'},
+    
+];
+
