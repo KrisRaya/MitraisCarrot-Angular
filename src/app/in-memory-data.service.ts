ed6d50f0cfeb1c474b8e97f816ce78eefa6577b3
nurcholis <nanda.nurcholis@mitrais.com>
2018-04-27 13:45:51 +0800

    edited for pbi7_v2

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,13 +2,13 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const employee = [
+    const employees = [
       { id : 1, name: 'Nanda' ,email: 'nurcholis.nanda@gmail.com'},
       { id : 2, name: "Jafar" ,email: 'jafarshodiqyb@gmail.com'},
       { id : 3, name: 'Kris' ,email: 'krisraya13@gmail.com'},
       { id : 4, name: 'Raisa' ,email: 'raisa6690@gmail.com'}
         
     ];
-    return {employee};
+    return {employees};
   }
 }
